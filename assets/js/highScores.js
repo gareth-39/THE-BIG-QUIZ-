d0871fdc1e9cd4aa28e2cab0e250469a9bcd92fb
gareth-39 <Gareth.A.Henvey@gmail.com>
2022-11-30 12:28:44 +0000

    making changes

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -1,13 +1,9 @@
-/* Js for the highscore page*/
+const username = document.getElementById('username');
+const saveButton = document.getElementById('saveScoreBtn');
+const highScoresList = document.querySelector('#highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const highScoresList = document.querySelector("#highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
-
-
-  
+highScoresList.innerHTML =
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join("")
