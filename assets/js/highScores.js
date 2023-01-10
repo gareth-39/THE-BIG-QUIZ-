443013cffe03cb3ea354227f8702fd7cf3b1fcec
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-01-10 11:17:19 +0000

    Ready to submit

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -4,6 +4,7 @@ const saveButton = document.getElementById('saveScoreBtn');
 const highScoresList = document.querySelector('#highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+/* For highscore list */
 highScoresList.innerHTML =
 highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`
