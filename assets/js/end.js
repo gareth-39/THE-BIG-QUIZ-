04c2215e0dcc48fb1a57d5729d3821f30e81bee7
gareth-39 <Gareth.A.Henvey@gmail.com>
2023-02-09 01:28:15 +0000

    Javascript code and commenting

--- assets/js/end.js
+++ assets/js/end.js
@@ -37,3 +37,38 @@ saveHighScore = e => {
             window.location.assign('highscores.html')
         }
 };
+
+let music = "off";
+const quizAudio = new Audio("assets/sounds/end.mp3.wav");
+
+
+quizAudio.loop = true;
+
+function whichMusic() { // Ability to play or pause audio to enhance experience on entering the quiz site
+
+    if (music === "on") {
+        quizAudio.play();
+    } else {
+        (music = "off")
+        quizAudio.pause();
+    }
+}
+
+function checkAudioButtons() {
+    if (music === "on") {
+        document.getElementById("audio").innerHTML = `<i class="fas fa-volume-mute"></i><br>Audio off`; // Changes the text of the button once clicked
+    } else {
+        document.getElementById("audio").innerHTML = `<i class="fas fa-volume-up"></i><br>Audio on`; // Changes the text of the button once clicked
+    }
+}
+
+// So that the user can toggle the music off or on
+function toggleMusic() { 
+    if (music === "off") {
+        music = "on";
+    } else {
+        music = "off";
+    }
+    checkAudioButtons();
+    whichMusic();
+}
